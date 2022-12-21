1adc60d888e10e1a78bba147791e02bd9fb50fd0
SsstassS38 <Stasonline26@gmail.com>
2022-12-21 13:20:17 +0300

    add Homework2

--- Python/Homework2/Example3.py
+++ Python/Homework2/Example3.py
@@ -1,26 +1,17 @@
 #Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
 # максимум использование библиотеки Random для получения случайного int
 
-# lst = [input('Введите список через разделитель-точку')]
 
 import random
 
 my_lst = []
-for i in range(10):
+for i in range(5):
     my_lst.append(random.randint(0, 100))
 print(f'случайный список: {my_lst}')
 
-newint = 0
-reslst = []
-for item in my_lst:
-    newint = random(item)
-    reslst.append(newint)
+for i in range(len(my_lst)-1, 0, -1):
+    j = random.randint(0, i - 1)
+    my_lst[i], my_lst[j] = my_lst[j], my_lst[i]
 
-
-print(f' Перемешанный список: {reslst}') 
-
-
-
-
-
+print(f'Перемешанный список: {my_lst}') 
 
